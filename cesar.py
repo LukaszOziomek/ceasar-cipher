ab4cd313f0637a3c6d3662e30df81cce0817ae1c
Lukasz Oziomek <lukaszoziomek@gmail.com>
2022-04-23 15:29:52 +0200

    poprawiona werjsa

--- cesar.py
+++ cesar.py
@@ -1,18 +1,40 @@
+"""
+Napisz funkcję encrypt, która zaszyfruje tekst zgodnie z szyfrem cesara.
+Oraz decrypt, która odkoduje ten tekst.
+    >>> encrypt("CEASER CIPHER DEMO", 4)
+    IEWIVrGMTLIVrHIQS
+    >>> decrypt("IEWIVrGMTLIVrHIQS", 4)
+    'CEASERzCIPHERzDEMO'
+Wersj trudniejsza
+    >>> decrypt("IEWIVrGMTLIVrHIQS", 4)
+    'CEASER CIPHER DEMO'
+przesuniecie 1
+"ABCD"
+"BCDA"
+przesuniecie 2
+"ABCD"
+"CDAB"
+"""
+
+
 def encrypt(text: str, shift: int) -> str:
+    """Cipher text with ceasar cipher"""
     result = ""
     for char in text:
         if char.isupper():
-            result += chr((ord(char) - shift - 65) % 26 + 65)
+            result += chr((ord(char) + shift - 65) % 26 + 65)
         else:
-            result += chr((ord(char) - shift - 97) % 26 + 97)
+            result += chr((ord(char) + shift - 97) % 26 + 97)
     return result
 
+
 def decrypt(text: str, shift: int) -> str:
+    """Decipher text with ceasar cipher"""
     result = ""
-    space = chr((ord(char) + shift - 65) % 26 + 65)
+    space = chr((ord(" ") + shift - 97) % 26 + 97)
     for char in text:
         if char == space:
-            result += ""
+            result += " "
         elif char.isupper():
             result += chr((ord(char) - shift - 65) % 26 + 65)
         else:
@@ -21,10 +43,9 @@ def decrypt(text: str, shift: int) -> str:
 
 
 if __name__ == "__main__":
-    assert encrypt("CEASER CIPHER DEMO", 4) == "GIEWIVrGMTLIVrHIOS"
+    assert encrypt("CEASER CIPHER DEMO", 4) == "GIEWIVrGMTLIVrHIQS"
     assert encrypt("A", 1) == "B"
-    assert decrypt("GIEWIVrGMTLIVrHIOS", 4) == "CEASER CIPHER DEMO"
+    assert decrypt("GIEWIVrGMTLIVrHIQS", 4) == "CEASER CIPHER DEMO"
     assert decrypt("B", 1) == "A"
 
-print("")
-
+print(decrypt("GIEWIVrGMTLIVrHIQS", 4))
